8a998a836ab2a1b58491233b4f19ea5e370e7b3c
Carl Villeneuve Lepage <carl.villeneuve-lepage.1@ulaval.ca>
2022-10-30 11:38:29 -0400

    correction fonction de départ

--- TP1_2009.R
+++ TP1_2009.R
@@ -1,5 +1,8 @@
-txrend <- (Donne_es_TP1_ACT_2009_Automne_2022)
+library(readxl)
+txrend <- Donne_es_TP1_ACT_2009_Automne_2022 <- read_excel(
+  "Documents/TP1_2009/Données TP1 ACT-2009 Automne 2022.xlsx")
 colnames(txrend) <- c("date", "ind_1", "ind_2")
+View(txrend)
 ## Attribution arbitraire des paramètres pour une matrice à 2 états :
 # État 1 = bonne condition
 #   ~N(mu1, sigma1) où mu1 > 0 car rendement positif
@@ -19,14 +22,18 @@ colnames(txrend) <- c("date", "ind_1", "ind_2")
 # haussemoy(txrend, 1) # calcule l la moyenne de la hausse de l'indice 1 
 #     lorsqu'elle augmente d'un mois è l 'autre
 
-haussemoy <- function(x, y)
-{
-  i <- 1
-  cumsum <- 0
-  n <- 0
-  for (i in seq_along(data))
-    if (x[i + 1, y + 1] > x[i, y + 1])
-      cumsum <- cumsum + x[i + 1, y + 1] - x[i, y + 1]
-      n <- n+1
-      cumsum / n
-}
+haussemoy <- function(x, y) mean(x[y+1] >= 0)
+
+# à ce point nous pouvons poser comme moyenne de départ, lorsqu'on est dans 
+# l'état 1 (bonne condition):
+mu11 <- haussemoy(txrend, 1) # mu indice 1, état 1
+mu21 <- haussemoy(txrend, 2) # mu indice 2, état 1
+
+## Similairement pour un point de départ dans l'état 2 (mauvaise condition)
+# nous pouvons créer une fonction calcule la moyenne de baisse
+# du tx de rendement lorsque celui-ci baisse :
+baissemoy <- function(x, y) mean(x[y+1] < 0)
+
+# et y assigné les variables :
+mu12 <- baissemoy(txrend, 1) # mu indice 1, état 2
+mu22 <- baissemoy(txrend, 2) # mu indice 2, état 2
